ff3acc4d8f906b2cf68030496a36784c9dec0b48
weberbry <bweber@intrepid.io>
2015-06-23 07:02:55 -0400

    Initial Commit

---
+++ NostalgiaMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NostalgiaMusic
+//
+//  Created by Bryan Weber on 6/23/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
