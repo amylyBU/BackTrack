7ea7ee03a9b6fccce6e6e7197e128a5aad40b8b9
weberbry <bweber@intrepid.io>
2015-06-23 07:13:44 -0400

    Add NMAHomeViewController

---
+++ NostalgiaMusic/Controllers/Home/NMAHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  NMAHomeViewController.h
+//  NostalgiaMusic
+//
+//  Created by Bryan Weber on 6/23/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NMAHomeViewController : UIViewController
+
+@end
