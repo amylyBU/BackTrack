c718bde4c7ca955210d4a1feb67f9bdbcca673d4
Amy Ly <amy@intrepid.io>
2015-07-15 13:45:23 -0400

    Refactored code for NMASettingsVC

--- NostalgiaMusic/Controllers/Settings/NMASettingsViewController.m
+++ NostalgiaMusic/Controllers/Settings/NMASettingsViewController.m
@@ -13,10 +13,26 @@
 
 static NSString * const kNMASettingsSwitchCellIdentifier = @"SettingsSwitchCell";
 static NSString * const kNMAFeedbackTableViewCellIdentifier = @"FeedbackTableViewCell";
+static NSInteger const kNumberOfSwitchTableCells = 2;
+static NSInteger const kNumberOfFeedbackTableCells = 1;
+static NSInteger const kNumberOfSections = 2;
+
+NS_ENUM(NSInteger, NMASettingsSectionType) {
+    NMASectionTypeSwitchCell,
+    NMASectionTypeFeedbackCell,
+};
+
+NS_ENUM(NSInteger, NMASwitchCellRowTagIdentifer) {
+    NMAFacebookConnectRowTag,
+    NMAAutoplayRowTag
+};
 
 @interface NMASettingsViewController () <UITableViewDataSource, UITableViewDelegate, NMASettingsSwitchCellDelegate>
+
 @property (weak, nonatomic) IBOutlet UITableView *staticTableView;
+
 @end
+
 
 @implementation NMASettingsViewController
 
@@ -24,7 +40,7 @@ static NSString * const kNMAFeedbackTableViewCellIdentifier = @"FeedbackTableVie
     [super viewDidLoad];
     self.edgesForExtendedLayout = UIRectEdgeNone;
     [self.staticTableView registerNib:[UINib nibWithNibName:NSStringFromClass([NMASettingsSwitchCell class]) bundle:nil]
-         forCellReuseIdentifier:kNMASettingsSwitchCellIdentifier];
+               forCellReuseIdentifier:kNMASettingsSwitchCellIdentifier];
     [self.staticTableView registerNib:[UINib nibWithNibName:NSStringFromClass([NMAFeedbackTableViewCell class]) bundle:nil]
                forCellReuseIdentifier:kNMAFeedbackTableViewCellIdentifier];
     self.staticTableView.delegate = self;
@@ -34,45 +50,46 @@ static NSString * const kNMAFeedbackTableViewCellIdentifier = @"FeedbackTableVie
 #pragma mark - UITableViewDelegate Methods
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 2;
+    return kNumberOfSections;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView
  numberOfRowsInSection:(NSInteger)section {
-    if(section == 0){
-        return 2;
-    } else {
-        return 1;
-    }
+    return section == NMASectionTypeSwitchCell ? kNumberOfSwitchTableCells : kNumberOfFeedbackTableCells;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section == 0) {
-        if (indexPath.row == 0) {
-            NMASettingsSwitchCell *facebookCell = [tableView dequeueReusableCellWithIdentifier:@"SettingsSwitchCell" forIndexPath:indexPath];
-            facebookCell.settingsCategoryLabel.text = @"Connect to Facebook";
-            facebookCell.settingsSwitch.on = [[NMAAppSettings sharedSettings] userIsLoggedIn];
-            facebookCell.delegate = self;
-            facebookCell.settingsSwitch.tag = 0;
-            return facebookCell;
-        } else {
-            NMASettingsSwitchCell *autoplayCell =  [tableView dequeueReusableCellWithIdentifier:kNMASettingsSwitchCellIdentifier forIndexPath:indexPath];
-            autoplayCell.settingsCategoryLabel.text = @"Autoplay";
-            autoplayCell.settingsSwitch.on = [[NMAAppSettings sharedSettings] userDidAutoplay];
-            autoplayCell.delegate = self;
-            autoplayCell.settingsSwitch.tag = 1;
-            return autoplayCell;
+    switch (indexPath.section) {
+        case NMASectionTypeSwitchCell: {
+            if (indexPath.row == NMAFacebookConnectRowTag) {
+                NMASettingsSwitchCell *facebookCell = [tableView dequeueReusableCellWithIdentifier:@"SettingsSwitchCell" forIndexPath:indexPath];
+                facebookCell.settingsCategoryLabel.text = @"Connect to Facebook";
+                facebookCell.settingsSwitch.on = [[NMAAppSettings sharedSettings] userIsLoggedIn];
+                facebookCell.delegate = self;
+                facebookCell.settingsSwitch.tag = NMAFacebookConnectRowTag;
+                return facebookCell;
+            } else {
+                NMASettingsSwitchCell *autoplayCell = [tableView dequeueReusableCellWithIdentifier:kNMASettingsSwitchCellIdentifier forIndexPath:indexPath];
+                autoplayCell.settingsCategoryLabel.text = @"Autoplay";
+                autoplayCell.settingsSwitch.on = [[NMAAppSettings sharedSettings] userDidAutoplay];
+                autoplayCell.delegate = self;
+                autoplayCell.settingsSwitch.tag = NMAAutoplayRowTag;
+                return autoplayCell;
+            }
         }
-    } else {
-        NMAFeedbackTableViewCell *feedbackCell =  [tableView dequeueReusableCellWithIdentifier:kNMAFeedbackTableViewCellIdentifier forIndexPath:indexPath];
-        feedbackCell.feedbackLabel.text = @"Feedback";
-        return feedbackCell;
+        case NMASectionTypeFeedbackCell: {
+            NMAFeedbackTableViewCell *feedbackCell = [tableView dequeueReusableCellWithIdentifier:kNMAFeedbackTableViewCellIdentifier forIndexPath:indexPath];
+            feedbackCell.feedbackLabel.text = @"Feedback";
+            return feedbackCell;
+        }
+        default:
+            return nil;
     }
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if(indexPath.section == 1) {
+    if (indexPath.section == NMASectionTypeFeedbackCell) {
         [self sendEmailFeedback];
     }
 }
@@ -86,17 +103,12 @@ static NSString * const kNMAFeedbackTableViewCellIdentifier = @"FeedbackTableVie
 }
 
 #pragma mark - Switch statement delegate method
+
 - (void)didPressSwitch:(UISwitch *)sender {
-    if (sender.tag == 0) {
+    if (sender.tag == NMAFacebookConnectRowTag) {
         NSLog(@"Facebook Settings Toggled. Settings are not saved.");
     } else {
-        if (sender.on) {
-            [[NMAAppSettings sharedSettings] setAutoplaySettingToOn];
-            
-        } else {
-            [[NMAAppSettings sharedSettings] setAutoplaySettingToOff];
-            
-        }
+        sender.on ? [[NMAAppSettings sharedSettings] setAutoplaySettingToOn] : [[NMAAppSettings sharedSettings] setAutoplaySettingToOff];
     }
 }
 
